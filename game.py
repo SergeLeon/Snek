7d70ea30275c81b985e01ee68ba9b4995c78d59d
Sergey Leonov <45981130+SergeLeon@users.noreply.github.com>
2020-11-15 18:13:42 +0300

    Upload

---
+++ game.py
@@ -0,0 +1,145 @@
+from random import randint
+
+import pygame
+
+import sound
+
+pygame.init()
+
+# размер окна
+size = [1280, 720]
+window = pygame.display.set_mode(size)
+
+pygame.display.set_caption("Snek game")
+
+screen = pygame.Surface(size)
+
+font = pygame.font.Font("AtariClassic-Regular.ttf", 36)
+
+cell_size = 40
+
+
+class Snake():
+    def __init__(self):
+        self.moves = {"hold": [0, 0],
+                      "up": [0, -1],
+                      "down": [0, 1],
+                      "left": [-1, 0],
+                      "right": [1, 0]}
+        self.score = 0
+        self.head = [16, 8]
+        self.snake = [[16, 10], [16, 9], self.head]
+        self.move_round = "up"
+
+    def render(self):
+        for block in self.snake:
+            pygame.draw.rect(screen, [0, 255, 0], (block[0] * cell_size, block[1] * cell_size, cell_size, cell_size))
+
+    def move(self):
+        pushed_keys = pygame.key.get_pressed()
+
+        if pushed_keys[pygame.K_w] and self.move_round != "down":
+            self.move_round = "up"
+        if pushed_keys[pygame.K_s] and self.move_round != "up":
+            self.move_round = "down"
+        if pushed_keys[pygame.K_d] and self.move_round != "left":
+            self.move_round = "right"
+        if pushed_keys[pygame.K_a] and self.move_round != "right":
+            self.move_round = "left"
+
+        self.head = [self.head[0] + self.moves[self.move_round][0], self.head[1] + self.moves[self.move_round][1]]
+        self.snake.append(self.head)
+
+        for apple in apple_list:
+            if self.check_apple_contact(apple):
+                apple.repose()
+                self.score += 1
+            else:
+                self.snake.pop(0)
+
+    def check_apple_contact(self, apple):
+        if self.head == apple.get_cords():
+            return True
+        return False
+
+    def check_death(self):
+        if self.head in self.snake[0:-1]:
+            return True
+        return False
+
+
+apple_list = []
+
+
+class Apple():
+    def __init__(self):
+        apple_list.append(self)
+        self.x = randint(1, 32)
+        self.y = randint(1, 18)
+        self.color = [255, 0, 0]
+
+    def render(self, cell_size):
+        pygame.draw.circle(screen, self.color,
+                           (self.x * cell_size + cell_size // 2 + 1, self.y * cell_size + cell_size // 2 + 1),
+                           cell_size // 2 - 1)
+
+    def get_cords(self):
+        return [self.x, self.y]
+
+    def repose(self):
+        self.x = randint(1, 32)
+        self.y = randint(1, 18)
+
+
+def render_field(window_size, cell_size):
+    depth = 2
+    for x in range(-1, round(window_size[0] / cell_size) + 1):
+        pygame.draw.line(screen, [0, 0, 0], [x * cell_size, 0], [x * cell_size, window_size[1]], depth)
+    for y in range(-1, round(window_size[1] / cell_size) + 1):
+        pygame.draw.line(screen, [0, 0, 0], [0, y * cell_size], [window_size[0], y * cell_size], depth)
+
+
+def show_score(score):
+    text = font.render(f"score: {score}", 1, (0, 0, 0))
+    text_pos = (442, 20)
+
+    screen.blit(text, text_pos)
+
+
+def start_game():
+    apple = Apple()
+    snek = Snake()
+
+    running_game = True
+    while running_game:
+        # обработка событий
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:
+                running_game = False
+
+        screen.fill([144, 238, 144])
+
+        if not snek.check_death():
+            for apple in apple_list:
+                apple.render(cell_size)
+
+            snek.move()
+
+            snek.render()
+
+            render_field(size, cell_size)
+            # отображение окна
+            window.blit(screen, [0, 0])
+        else:
+            text = font.render("You Lose", 1, (0, 0, 0))
+            screen.blit(text, (445, 300))
+
+        window.blit(screen, [0, 0])
+        pygame.display.flip()
+        pygame.time.delay(100)
+
+    pygame.display.set_caption("SneK menu")
+
+
+if __name__ == "__main__":
+    start_game()
---
+++ menu.py
@@ -0,0 +1,98 @@
+import webbrowser
+from collections import namedtuple
+from random import randint
+
+import pygame
+
+import game
+import sound
+
+pygame.init()
+pygame.mixer.init(buffer=16)
+
+# размер окна
+size = [1280, 720]
+window = pygame.display.set_mode(size)
+# задайте имя
+pygame.display.set_caption("SneK menu")
+
+screen = pygame.Surface(size)
+
+font = pygame.font.Font("AtariClassic-Regular.ttf", 36)
+
+
+def check_touch(cords, block_cords, block_size):
+    """checks whether the coordinate is within other coordinates"""
+    if cords[0] in range(block_cords[0], (block_cords[0] + block_size[0] + 1)) \
+            and cords[1] in range(block_cords[1], (block_cords[1] + block_size[1] + 1)):
+        return True
+    return False
+
+
+# создание кнопок
+Button = namedtuple("Button", ["surface", "size", "cords", "text", "action"])
+
+button_play = Button(surface=pygame.Surface([400, 100]), size=[400, 100], cords=[440, 260],
+                     text=["Play", [440 + 130, 260 + 30]],
+                     action=f"game.start_game()")
+button_exit = Button(surface=pygame.Surface([400, 100]), size=[400, 100], cords=[440, 380],
+                     text=["Exit", [440 + 130, 380 + 30]],
+                     action="running_menu = False")
+button_volume = Button(surface=pygame.Surface([250, 100]), size=[250, 100], cords=[20, 600],
+                       text=["+Sound+", [19, 630]],
+                       action="""if button.text[0]=='+Sound+':
+                                    sound.set_volume(0)
+                                    button.text[0]='-Sound-'
+                                \nelse:
+                                    sound.set_volume(1)
+                                    button.text[0]='+Sound+'""")
+button_turn_player_count = Button(surface=pygame.Surface([100, 100]), size=[100, 100], cords=[1160, 600],
+                                  text=["1P", [1175, 630]],
+                                  action="""if button.text[0]=='1P':
+                                        player_count = 2
+                                        button.text[0] = '2P'
+                                    \nelif button.text[0]=='2P':
+                                        player_count = 0
+                                        button.text[0] = 'AI'
+                                    \nelse:
+                                        player_count = 1
+                                        button.text[0] = '1P'""")
+
+buttons_list = (button_play, button_exit, button_volume, button_turn_player_count)
+
+running_menu = True
+while running_menu:
+
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            exit()
+
+    screen.fill([144, 238, 144])
+
+    # ссылка на создателя
+    if check_touch(pygame.mouse.get_pos(), [0, 0], [230, 50]):
+        screen.blit(pygame.font.Font("AtariClassic-Regular.ttf", 16).render("Creator:", 1, (0, 0, 0)), (0, 0))
+        screen.blit(pygame.font.Font("AtariClassic-Regular.ttf", 24).render("SergeLeon", 1, (
+            randint(0, 255), randint(0, 255), randint(0, 255))), (0, 20))
+        if pygame.mouse.get_pressed()[0]:
+            webbrowser.open("https://github.com/SergeLeon", new=2)
+
+    screen.blit(pygame.font.Font("AtariClassic-Regular.ttf", 64).render("SneK", 1, (0, 0, 0)), (510, 100))
+
+    # работа с кнопками
+    for button in buttons_list:
+        if check_touch(pygame.mouse.get_pos(), button.cords, button.size):
+            button.surface.fill([0, 0, 0])
+            screen.blit(button.surface, button.cords)
+            screen.blit(font.render(button.text[0], 1, [100, 100, 100]), button.text[1])
+            if pygame.mouse.get_pressed()[0]:
+                # sound.play_button_press_sound()
+                exec(button.action)
+        else:
+            button.surface.fill([100, 100, 100])
+            screen.blit(button.surface, button.cords)
+            screen.blit(font.render(button.text[0], 1, (0, 0, 0)), button.text[1])
+
+    window.blit(screen, [0, 0])
+    pygame.display.flip()
+    pygame.time.delay(100)
